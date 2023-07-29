f0f4e3c2a8fbae1b58626f471e9e9f0e074a9c32
kitayikuyo <2067878944@qq.com>
2023-07-29 20:07:43 +0800

    2023/07/29

---
+++ 线性表(顺序存储结构)/delete(删除非必要文件).bat
@@ -0,0 +1,6 @@
+REM 清理C++项目临时文件
+del *.sdf *.user *.filters *.ipch *.aps /s
+del *.obj *.pch *.vc.db /s
+del *.exe *.dll /s
+del *.suo /s /a h
+del *ilk *.pdb *.exp *.lib *.tlog *.manifest *.res *.lastbuildstate /s
